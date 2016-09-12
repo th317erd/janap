ae50caf881c0e74ab108431ade50e9cbd4090f03
Wyatt Greenway <wyatt@mindnest.com>
2016-09-12 14:31:15 -0700

    Initial push

--- README.md
+++ README.md
@@ -1,2 +1,108 @@
-# janop
-Just another Node Argument Parser
+# janap
+
+Just Another Node Argument Parser is a super small and super flexible command line argument parser for node
+
+I wrote this because all other argument parsers out there were large, had dependencies, or didn't allow customization
+
+# Install
+    npm install janap
+
+# Use
+    var janap = require('janap');
+    var args = janap.parse(process.argv);
+
+    //args contain parsed arguments
+    //i.e. node myscript.js --arg1=false --boolean -hello world
+    // = {arg1: false, boolean: true, hello: "world"}
+
+# Skip Initial parsing (argument 0 and 1)
+    var janap = require('janap');
+    var args = janap.parse(process.argv.slice(2), {
+      _initial: false
+    });
+
+# Data Types
+With Janap you can force a data type per-argument:
+    var janap = require('janap');
+    var args = janap.parse(process.argv, {
+    	force: Boolean,
+    	count: Number,
+    	paths: Array
+    });
+
+    //note: you can also use strings such as "bool", "boolean", "number", "array", or something custom
+
+# Aliases
+   var janap = require('janap');
+   var args = janap.parse(process.argv, {
+     _alias: {
+       'o': 'option'
+     }
+   });
+
+   //Arguments of "--o=value" or "-o value" will result in "args" object of {option: value}
+
+# Customize
+Janap was built with customization in mind. You can override 'match' to specify your own argument format:
+    var janap = require('janap');
+    janap.match = function(arg) {
+      //We want triple dashes on our arguments
+      //the second match is always the value
+      //i.e. ---argument=value
+      return ('' + arg).match(/^---([\w-]+)(?:=(.*))?$/);
+    }
+
+    var args = janap.parse(process.argv);
+
+# Custom type converters
+With Janap you can specify custom type converters. Just pass in a "_converter" key to the options object to specify your own custom converter:
+    var janap = require('janap');
+
+    function MyArgumentConverter() {
+    	janap.ArgumentConverter.call();
+    }
+
+    var p = MyArgumentConverter.prototype = Object.create(ArgumentConverter.prototype);
+    p.constructor = MyArgumentConverter;
+    p.convertValue = function(value, type) {
+    	if (this.name === 'my-special-case-argument')
+    		return 'some other value';
+
+    	//Custom data-type, specified through {'my-argument': 'custom'} in the options object
+    	if (type === 'custom')
+    		return myCustomValueConverter(value);
+
+    	if (type === 'number')
+    		return this.toNumber(value);
+
+    	if (type === 'boolean')
+    		return this.toBoolean(value);
+
+    	//this.continue tells the parser to use this value,
+    	//but continue onto with the next argument using the same "name" / key
+    	if (type === 'array') {
+    		var currentValue = this.get(this.name);
+    		if (currentValue === undefined) {
+    			//Not set yet
+    			return this.continue([value]);
+    		}
+
+    		return this.continue(currentValue.concat(value));
+    	}
+
+    	if (this.name === 'do-stuff') {
+    		//This adds/sets an argument called "doStuffRequested" to true in the final arguments object
+    		this.set('doStuffRequested', true);
+    		return value;
+    	}
+
+      //Default value converter
+      //super is a convenience helper that is always set to "ArgumentConverter.prototype"
+    	return this.super.convertValue(value, type);
+    };
+
+    var args = janap.parse(process.argv, {
+      _converter: new MyArgumentConverter()
+    });
+
+# Enjoy! :)
---
+++ index.js
@@ -0,0 +1,231 @@
+// (c) 2016 Wyatt Greenway
+// This code is licensed under MIT license (see LICENSE.txt for details)
+
+(function(factory) {
+	module.exports = factory({});
+})(function(root) {
+	function ArgumentParserContinue(val) {
+		this.value = val;
+	}
+
+	ArgumentParserContinue.prototype = {
+		constructor: ArgumentParserContinue
+	};
+
+	function getType(val) {
+		if (val === undefined || val === null)
+			return 'undefined';
+
+		if (val === Boolean || val === 'boolean' || val === 'bool' || val instanceof Boolean)
+			return 'boolean';
+
+		if (val === Array || val === 'array' || val instanceof Array)
+			return 'array';
+
+		if (val === Number || val === 'number' || val instanceof Number)
+			return 'number';
+
+		if (val === String || val === 'string' || val instanceof String)
+			return 'string';
+
+		return val;
+	}
+
+	function ArgumentConverter() {
+		this.index = 0;
+		this.name = '';
+		this.context = {};
+		this.options = {};
+		this.super = root.ArgumentConverter.prototype;
+	}
+
+	ArgumentConverter.prototype = {
+		constructor: ArgumentConverter,
+		continue: function(value) {
+			return new ArgumentParserContinue(value);
+		},
+		coerce: function(value) {
+			var number = parseFloat(value);
+			if (!isNaN(number) && isFinite(number))
+				return number;
+
+			if (('' + value).match(/(true|yes)/i))
+				return true;
+
+			if (('' + value).match(/(false|no)/i))
+				return false;
+
+			if (value === undefined)
+				return true;
+
+			return value;
+		},
+		toNumber: function(value) {
+			var number = parseFloat(value);
+			if (!isNaN(number) && isFinite(number))
+				return number;
+
+			return 0;
+		},
+		toBoolean: function(value) {
+			if (('' + value).match(/(true|yes)/i))
+				return true;
+
+			if (('' + value).match(/(false|no)/i))
+				return false;
+
+			return true;
+		},
+		convertValue: function(_value, _type) {
+			var type = _type || 'undefined',
+					value = _value;
+
+			switch(type) {
+				case 'array':
+					if (('' + value).match(/^\s*\[/)) {
+						var parts = ('' + value).trim().replace(/^.*?\[/,'').replace(/\].*$/,'').split(/[\s,]+\s*/g),
+								finalArray = [];
+
+						for (var i = 0, il = parts.length; i < il; i++)
+							finalArray.push(this.coerce(parts[i]));
+
+						value = finalArray;
+					}
+
+					if (!(value instanceof Array))
+						value = this.coerce(value);
+
+					if (!(value instanceof Array))
+						value = [value];
+
+					var currentValue = this.get(this.name);
+					if (currentValue === undefined)
+						return this.continue(value);
+
+					return this.continue(currentValue.concat(value));
+				case 'boolean':
+					return this.toBoolean(value);
+				case 'number':
+					return this.toNumber(value);
+				default:
+					return this.coerce(value);
+			}
+		},
+		convert: function(value) {
+			var name = this.name,
+					type = getType(this.options[name]);
+
+			return this.convertValue(value, type);
+		},
+		set: function(name, value) {
+			this.context[name] = value;
+		},
+		get: function(name) {
+			return this.context[name];
+		}
+	};
+
+	function match(previous, arg) {
+		return ('' + arg).match(/^--?([\w-]+)(?:=(.*))?$/);
+	}
+
+	function alias(key, aliases) {
+		var alias = aliases[key];
+		return (!alias) ? key : alias;
+	}
+
+	function parse(_args, _opts) {
+		var json = {},
+				opts = _opts || {},
+				args = _args,
+				initial = opts._initial,
+				key = undefined, value = undefined,
+				indexKey = 0,
+				converter = opts._converter;
+
+		if (!args || !args.length)
+			return json;
+
+		if (!(converter instanceof root.ArgumentConverter) && converter instanceof Function)
+			converter = new converter();
+		else if (!converter)
+			converter = new root.ArgumentConverter();
+
+		converter.options = opts;
+		converter.context = json;
+
+		for (var i = 0, il = args.length; i < il; i++) {
+			var arg = args[i];
+			
+			if (initial !== false && i === 0) {
+				json._env = arg;
+				continue;
+			} else if (initial !== false && i === 1) {
+				json._entry = arg;
+				continue;
+			}
+
+			converter.index = i;
+
+			var parts = root.match.call(json, key, arg);
+			if (!parts) {
+				if (key) {
+					value = arg;
+				} else {
+					json[indexKey++] = arg;
+					continue;	
+				}
+			} else {
+				if (key && json[key] === undefined)
+					json[key] = true;
+
+				key = parts[1];
+				key = root.alias.call(json, key, opts._alias || {});
+				converter.name = key;
+
+				if (parts.length > 2 && parts[2] !== undefined) {
+ 					value = converter.convert(parts[2]);
+
+ 					json[key] = (value instanceof ArgumentParserContinue) ? value.value : value;			
+					
+					key = undefined;
+				} else {
+					json[key] = undefined;
+				}
+
+				value = undefined;
+				continue;
+			}
+
+			converter.name = (!key) ? i : key;
+ 			value = converter.convert(value);
+
+			if (!key) {
+				json[indexKey++] = (value instanceof ArgumentParserContinue) ? value.value : value;
+			} else {
+				if (value instanceof ArgumentParserContinue) {
+					json[key] = value.value;
+					value = undefined;
+					continue;
+				}
+
+				json[key] = value;
+				key = undefined;
+			}
+
+			value = undefined;
+		}
+
+		if (key && json[key] === undefined)
+			json[key] = true;
+
+		return json;
+	}
+
+	root.ArgumentConverter = ArgumentConverter;
+	root.match = match;
+	root.alias = alias;
+	root.parse = parse;
+
+	return root;
+});
